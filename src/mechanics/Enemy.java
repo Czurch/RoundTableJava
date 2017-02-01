23aed7894d818703b0b943f1e7a950e01570f923
Czurch <lukesferreira1217@gmail.com>
2017-02-01 00:45:22 -0500

    Core Reconstruction

---
+++ src/mechanics/Enemy.java
@@ -0,0 +1,55 @@
+package mechanics;
+
+public class Enemy {
+	//Base stats for player
+		String name;
+	    private int health 
+	    ,maxHealth
+	    ,attack
+	    ,defence
+	    ,initiative
+	    ,initMod;
+	    boolean alive;
+		    
+	    public Enemy(){
+	    	name = "Placeholder";
+	    	health = 8;
+	    	maxHealth = 8;
+	    	attack = 0;
+	    	defence = 0;
+	    	initiative = 0;
+	    	initMod = 0;
+	    	alive = true;
+	    }
+	    
+	    public Enemy(String nam, int HP, int ATK, int DEF, int INIT){
+	    	name = nam;
+	    	health = HP;
+	    	maxHealth = HP;
+	    	attack = ATK;
+	    	defence = DEF;
+	    	initiative = 0;
+	    	initMod = INIT;
+	    	alive = true;
+	    }
+	    
+	    public int attackMove(Enemy target){
+	    	int damage = attack + coreMath.rollDie();
+	    	return damage;
+	    }
+	    
+		   //Causes enemy to lose health on attack
+	    public void takeDamage(int nmeATK){
+	        int damage = (nmeATK - defence);
+	        if(damage > 0)										
+	        	health -= damage;
+	        if(health <= 0)
+	        	health -= damage;					//if the damage would kill, set alive to false
+	        	alive = false;
+	    }
+	    
+	    public void rollInit(){
+	    	initiative = coreMath.rollDie() + initMod;
+	    }
+}
+
